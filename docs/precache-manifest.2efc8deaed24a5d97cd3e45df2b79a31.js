110b7f554e5a7c5e7038cc77de45b2df67fdc947
jackson-ruiz <jackson-ruiz@thoughtworks.com>
2022-04-11 16:44:24 -0500

    docs agregado

---
+++ docs/precache-manifest.2efc8deaed24a5d97cd3e45df2b79a31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61318ccbb2026531a9c52457694ce2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3905c4a4c9382751f83",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3905c4a4c9382751f83",
+    "url": "/static/js/main.d3b43fe8.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
